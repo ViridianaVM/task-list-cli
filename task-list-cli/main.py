7b0f220e051e16cc920bb6926d463a72e8083b29
Becca <becca@adadevelopersacademy.org>
2021-05-02 15:34:27 -0700

    removed extra directory

--- task-list-cli/main.py
+++
@@ -1,9 +0,0 @@
-from task_list_cli.task_list import TaskList
-
-becca = task_list()
-
-new_task = becca.create_task(title="learn flask", description="learn it")
-print(new_task)
-
-becca.list_tasks()
-
